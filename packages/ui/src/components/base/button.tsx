73733ea37881b61a9ac8b6f67d2d10448645184f
Sutaenghhh <neocop7@gmail.com>
2025-05-26 14:24:26 +0900

    feat: button 공통 컴포넌트 생성

--- packages/ui/src/components/base/button.tsx
+++ packages/ui/src/components/base/button.tsx
@@ -4,25 +4,20 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '../../lib/utils';
 
 const buttonVariants = cva(
-    "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+    "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-bold transition-all disabled:bg-gray-300 disabled:text-white disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
     {
         variants: {
             variant: {
                 default:
-                    'bg-primary text-primary-foreground shadow-xs hover:bg-primary/90',
-                destructive:
-                    'bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
+                    'bg-primary-100 text-white shadow-xs hover:bg-primary-100/90 cursor-pointer',
                 outline:
-                    'border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50',
-                secondary:
-                    'bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80',
-                ghost: 'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
-                link: 'text-primary underline-offset-4 hover:underline',
+                    'border border-primary-100 text-primary-100 shadow-xs hover:bg-primary-100/10 active:bg-primary-100/10 cursor-pointer',
+                text: 'text-primary-100 hover:bg-primary-100/10 active:bg-primary-100/10 cursor-pointer',
             },
             size: {
-                default: 'h-9 px-4 py-2 has-[>svg]:px-3',
-                sm: 'h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5',
-                lg: 'h-10 rounded-md px-6 has-[>svg]:px-4',
+                default: 'px-3 py-[6px] has-[>svg]:px-3 w-full rounded-lg',
+                sm: 'rounded-md gap-1.5 px-3 has-[>svg]:px-2.5',
+                lg: 'rounded-lg px-6 has-[>svg]:px-4',
                 icon: 'size-9',
             },
         },
