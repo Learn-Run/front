e81c066caae6beb8df1d64d1a830eeffedc03329
Sutaenghhh <neocop7@gmail.com>
2025-05-27 12:00:30 +0900

    refactor: navlist  상수 분리

--- apps/pick-learn/src/widgets/layout/ui/BottomNavBar.tsx
+++ apps/pick-learn/src/widgets/layout/ui/BottomNavBar.tsx
@@ -1,45 +1,12 @@
 'use client';
 import { usePathname } from 'next/navigation';
-import { cn } from '@repo/ui/lib/utils';
 import Link from 'next/link';
 
-import { routes } from '@/shared/constants/routes';
-import { Chat, Home, PopularPost, Search, Post } from '@/shared/assets/icons';
+import { cn } from '@repo/ui/lib/utils';
+import { navList } from '../contants';
 
 export default function BottomNavBar() {
     const pathname = usePathname();
-
-    const navList = [
-        { id: 1, name: 'Home', href: routes.home, isAuth: false, icon: Home },
-        {
-            id: 2,
-            name: '질문보기',
-            href: routes.post,
-            isAuth: false,
-            icon: Post,
-        },
-        {
-            id: 3,
-            name: '인기질문',
-            href: routes.popularPosts,
-            isAuth: false,
-            icon: PopularPost,
-        },
-        {
-            id: 4,
-            name: '질문찾기',
-            href: routes.searchPosts,
-            isAuth: false,
-            icon: Search,
-        },
-        {
-            id: 5,
-            name: '소통해요',
-            href: routes.chat,
-            isAuth: true,
-            icon: Chat,
-        },
-    ];
 
     return (
         <nav className='fixed bottom-0 left-0 w-full bg-white md:hidden border-t py-2 z-50'>
--- apps/pick-learn/src/widgets/layout/ui/Nav.tsx
+++ apps/pick-learn/src/widgets/layout/ui/Nav.tsx
@@ -1,40 +1,12 @@
 'use client';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { cn } from '@repo/ui/lib/utils';
 
-import { routes } from '@/shared/constants/routes';
+import { cn } from '@repo/ui/lib/utils';
+import { navList } from '../contants';
 
 export default function Nav({ className }: { className?: string }) {
     const pathname = usePathname();
-
-    const navList = [
-        { id: 1, name: 'Home', href: routes.home, isAuth: false },
-        {
-            id: 2,
-            name: '질문보기',
-            href: routes.post,
-            isAuth: false,
-        },
-        {
-            id: 3,
-            name: '인기질문',
-            href: routes.popularPosts,
-            isAuth: false,
-        },
-        {
-            id: 4,
-            name: '질문찾기',
-            href: routes.searchPosts,
-            isAuth: false,
-        },
-        {
-            id: 5,
-            name: '소통해요',
-            href: routes.chat,
-            isAuth: true,
-        },
-    ];
 
     return (
         <nav className={cn('sticky md:static', className)}>
