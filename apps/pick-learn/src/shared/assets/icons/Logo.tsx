9abdc7bb69bb88c75876aa3c2234bb681fda3417
Sutaenghhh <neocop7@gmail.com>
2025-05-27 11:40:05 +0900

    refactor: logo className 받도록 수정

--- apps/pick-learn/src/shared/assets/icons/Logo.tsx
+++ apps/pick-learn/src/shared/assets/icons/Logo.tsx
@@ -1,9 +1,16 @@
 import Image from 'next/image';
+
+import { cn } from '@repo/ui/lib/utils';
 import logo from '@/shared/assets/icons/logo.png';
 
-export default function Logo() {
+export default function Logo({ className }: { className?: string }) {
     return (
-        <h1 className='relative w-full min-w-[120px] max-w-[150px] 2xl:max-w-[200px] h-[50px]'>
+        <h1
+            className={cn(
+                'relative w-full min-w-[120px] max-w-[150px] 2xl:max-w-[200px] h-[50px]',
+                className,
+            )}
+        >
             <Image
                 src={logo.src}
                 alt='logo'
