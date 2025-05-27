58b4d02766ebf30246729515f169d602ff2e0b58
Sutaenghhh <neocop7@gmail.com>
2025-05-27 16:36:49 +0900

    feat: 카테고리 탭 추가
    - widgets/layout/contants.ts 파일에 카테고리 추가
    - routes.ts에 카테고리 경로 추가

--- apps/pick-learn/src/widgets/layout/contants.ts
+++ apps/pick-learn/src/widgets/layout/contants.ts
@@ -32,3 +32,69 @@ export const navList = [
         icon: Chat,
     },
 ];
+
+export const postSubMenu = [
+    { id: 1, name: '전체', href: routes.categoryAll },
+    {
+        id: 2,
+        name: '예체능',
+        href: routes.categoryArticle,
+        detailMenu: [
+            { id: 0, name: '전체', href: routes.categoryArticle },
+            { id: 1, name: '운동', href: routes.categoryFitness },
+            { id: 2, name: '음악', href: routes.categoryMusic },
+            { id: 3, name: '댄스', href: routes.categoryDance },
+        ],
+    },
+    {
+        id: 3,
+        name: 'IT',
+        href: routes.categoryIt,
+        detailMenu: [
+            { id: 0, name: '전체', href: routes.categoryIt },
+            { id: 1, name: '디자인', href: routes.categoryDesign },
+            { id: 2, name: '개발', href: routes.categoryDevelopment },
+            { id: 3, name: '마케팅', href: routes.categoryMarketing },
+            { id: 4, name: '영상제작', href: routes.categoryVideoProduction },
+        ],
+    },
+    {
+        id: 4,
+        name: '취업',
+        href: routes.categoryJobs,
+        detailMenu: [
+            { id: 0, name: '전체', href: routes.categoryJobs },
+            { id: 1, name: '면접', href: routes.categoryJobInterview },
+            {
+                id: 2,
+                name: '포폴/이력서 검토',
+                href: routes.categoryPortfolioResumeReview,
+            },
+        ],
+    },
+    {
+        id: 5,
+        name: '요리',
+        href: routes.categoryCook,
+        detailMenu: [
+            { id: 0, name: '전체', href: routes.categoryCook },
+            { id: 1, name: '한식', href: routes.categoryKoreanCuisine },
+            { id: 2, name: '양식', href: routes.categoryWesternCuisine },
+            { id: 3, name: '중식', href: routes.categoryChineseCuisine },
+            { id: 4, name: '일식', href: routes.categoryJapaneseCuisine },
+            { id: 5, name: '베이커리', href: routes.categoryBakery },
+        ],
+    },
+    { id: 6, name: '인테리어', href: routes.categotryInterior },
+    {
+        id: 7,
+        name: '의류/뷰티',
+        href: routes.categoryFashion,
+        detailMenu: [
+            { id: 0, name: '전체', href: routes.categoryFashion },
+            { id: 1, name: '코디', href: routes.categoryCody },
+            { id: 2, name: '메이크업', href: routes.categoryMakeup },
+            { id: 3, name: '헤어', href: routes.categoryHair },
+        ],
+    },
+];
