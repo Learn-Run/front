c08be2142e391901f7a677b5d40c101eb982fdaa
Dora_bang <39180932+Dorabang@users.noreply.github.com>
2025-05-28 18:01:36 +0900

    feat: auth navigator (#39)
    * feat: auth 모달에서 쓰이는 navigator 컴포넌트 추가
    
    - 뒤로가기가 가능한지 아닌지를 리턴하는 canGoBack 함수 분리
    - Back, Close 아이콘 파일 추가
    
    * fix: button 컴포넌트에 shrink css 삭제
    
    - shrink css로 인해 width를 전체로 차지하기 때문에 해당 css는 cn을 사용해서 별도로 설정할 수 있도록 하는 것이 좋을 것으로 보임
    
    * fix: navigator를 client 컴포넌트로 선언
    
    - 파일 최상단에 'use client' 선언 추가

---
+++ apps/pick-learn/src/widgets/auth/ui/Navigator.tsx
@@ -0,0 +1,64 @@
+'use client';
+import Back from '@/shared/assets/icons/Back';
+import Close from '@/shared/assets/icons/Close';
+import { useModalContext } from '@/shared/model/modal/ModalContext';
+import Stepper from '@repo/ui/components/base/Stepper';
+import { cn } from '@repo/ui/lib/utils';
+import { canGoBack } from '../utils';
+
+type NavigationProps = {
+    step: number;
+    setStep: React.Dispatch<React.SetStateAction<number>>;
+    totalStepCount: number;
+    initStep?: number;
+};
+
+export default function Navigator({
+    step,
+    setStep,
+    totalStepCount,
+    initStep = 1,
+}: NavigationProps) {
+    const { closeModal } = useModalContext();
+
+    function handleBack(
+        step: number,
+        setStep: React.Dispatch<React.SetStateAction<number>>,
+        initStep: number,
+    ) {
+        if (canGoBack(step, initStep)) {
+            setStep((prev) => prev - 1);
+        }
+    }
+
+    return (
+        <section
+            className={cn(
+                'flex items-center',
+                !canGoBack(step, initStep) ? 'justify-end' : 'justify-between',
+            )}
+        >
+            <button
+                type='button'
+                disabled={!canGoBack(step, initStep)}
+                className={cn(
+                    !canGoBack(step, initStep) ? 'hidden disabled:point-' : '',
+                )}
+                onClick={() => handleBack(step, setStep, initStep)}
+            >
+                <Back />
+            </button>
+
+            {step > initStep && (
+                <Stepper
+                    currentStep={step - 1}
+                    totalSteps={totalStepCount - 1}
+                />
+            )}
+
+            <button type='button' onClick={() => closeModal()}>
+                <Close />
+            </button>
+        </section>
+    );
+}
