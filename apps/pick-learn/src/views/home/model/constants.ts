ed4eeb04247f839243c40a932b96aa9ddbdb6432
Sutaenghhh <neocop7@gmail.com>
2025-05-28 17:39:19 +0900

    feat: main step card 생성
    - constants.ts 에서 stepList 상수 분리
    - stepSection.tsx 에서 stepSection UI 생성
    - stepCard.tsx 에서 카드 UI 생성

---
+++ apps/pick-learn/src/views/home/model/constants.ts
@@ -0,0 +1,25 @@
+import { PopularAsker, StepAnswer, StepSearch } from '@/shared/assets/icons';
+
+export const stepList = [
+    {
+        id: 1,
+        title: '인기 질문자',
+        description:
+            'Upload your resume, fill in your skills, and set job preferences',
+        icon: PopularAsker,
+    },
+    {
+        id: 2,
+        title: '검색 및 찜하기 ',
+        description:
+            'Explore thousands of job listings and apply with one click',
+        icon: StepSearch,
+    },
+    {
+        id: 3,
+        title: '답변하기',
+        description:
+            'Schedule interviews, negotiate offers, and start your new job',
+        icon: StepAnswer,
+    },
+];
