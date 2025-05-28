3227ef4f59ff445518868f490611c2774c5143ac
Sutaenghhh <neocop7@gmail.com>
2025-05-28 18:09:09 +0900

    feat: carousel 생성

--- apps/pick-learn/src/app/(home)/page.tsx
+++ apps/pick-learn/src/app/(home)/page.tsx
@@ -1,5 +1,11 @@
 import StepSection from '@/views/home/ui/StepSection';
 import Heading from '@/widgets/home/ui/Heading';
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from '@repo/ui/components/base/Accordion';
 
 export default function Home() {
     return (
@@ -16,6 +22,16 @@ export default function Home() {
                 </Heading.SubTitle>
             </Heading>
             <StepSection />
+            <Accordion type='single' collapsible className='mt-10 mb-10'>
+                <AccordionItem value='item-1'>
+                    <AccordionTrigger>Item 1</AccordionTrigger>
+                    <AccordionContent>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua.
+                    </AccordionContent>
+                </AccordionItem>
+            </Accordion>
         </main>
     );
 }
