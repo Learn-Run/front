d37599134a3804fdb4a2eabe8f5368541663899a
Sutaenghhh <neocop7@gmail.com>
2025-05-28 11:14:21 +0900

    refactor: cn으로 tag 스타일 확장성 높임

--- apps/pick-learn/src/shared/ui/Tag.tsx
+++ apps/pick-learn/src/shared/ui/Tag.tsx
@@ -1,6 +1,19 @@
-export default function Tag({ children }: { children: React.ReactNode }) {
+import { cn } from '@repo/ui/lib/utils';
+
+export default function Tag({
+    children,
+    className,
+}: {
+    children: React.ReactNode;
+    className?: string;
+}) {
     return (
-        <div className='border border-gray-500 rounded-lg w-fit px-3 py-1.5 font-semibold text-gray-500'>
+        <div
+            className={cn(
+                'border border-gray-500 rounded-lg w-fit px-3 py-1.5 font-semibold text-gray-500',
+                className,
+            )}
+        >
             {children}
         </div>
     );
