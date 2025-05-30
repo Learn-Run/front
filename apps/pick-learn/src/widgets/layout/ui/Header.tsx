9473b6f60bb76763513f6ea8c5e34f994841eb85
Sutaenghhh <neocop7@gmail.com>
2025-05-30 17:22:26 +0900

    refactor: layout css 수정
    - footer css 수정
    - header css 수정
    - nav css 수정

--- apps/pick-learn/src/widgets/layout/ui/Header.tsx
+++ apps/pick-learn/src/widgets/layout/ui/Header.tsx
@@ -4,27 +4,37 @@ import { Button } from '@repo/ui/components/base/Button';
 import Logo from '@/shared/assets/icons/Logo';
 import Nav from './Nav';
 import LogIn from '@/shared/assets/icons/LogIn';
+import { cn } from '@repo/ui/lib/utils';
 
 export default function Header() {
     return (
-        <header className='grid grid-cols-12 items-center container mx-auto py-5 px-5 justify-between'>
-            <Link href='/' className='col-span-6 md:col-span-3 '>
-                <Logo />
-            </Link>
-            <Nav className='hidden md:block col-span-6 justify-self-center' />
-            <div className='col-span-6 md:col-span-3 flex justify-end items-center gap-2'>
-                <Button variant='text' className='w-fit hidden md:block'>
-                    log in
-                </Button>
-                <Button
-                    variant='outline'
-                    className='w-fit hidden md:block border-primary-100 text-primary-100'
+        <header
+            className={cn(
+                'fixed top-0 left-0 z-50 w-full bg-white/10 backdrop-blur-xs',
+            )}
+        >
+            <div className='grid grid-cols-12 items-center container mx-auto px-4 2xl:px-0 h-[5rem]'>
+                <Link
+                    href='/'
+                    className='col-span-6 md:col-span-2 xl:col-span-3'
                 >
-                    Register
-                </Button>
-                <Link href='/login'>
-                    <LogIn className='md:hidden' />
+                    <Logo />
                 </Link>
+                <Nav className='hidden md:block col-span-6 md:col-span-8 xl:col-span-6' />
+                <div className='col-span-6 md:col-span-2 xl:col-span-3 flex justify-end items-center gap-2'>
+                    <Button variant='text' className='w-fit hidden md:block'>
+                        log in
+                    </Button>
+                    <Button
+                        variant='outline'
+                        className='w-fit hidden md:block border-primary-100 text-primary-100'
+                    >
+                        Register
+                    </Button>
+                    <Link href='/login' className='w-fit md:hidden'>
+                        <LogIn />
+                    </Link>
+                </div>
             </div>
         </header>
     );
