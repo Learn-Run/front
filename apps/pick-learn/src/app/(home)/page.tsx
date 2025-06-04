85ec8e79eb708133a09dc3e889f01a3400531a6f
Sutaenghhh <neocop7@gmail.com>
2025-06-04 11:51:20 +0900

    refactor: 카테고리 api연결 및 타입 수정  (#59)
    * refactor: maincategory api 연결 및 subcategory 타입 수정
    
    * refactor: topasksection 카테고리 선택시 css 변경 및 searchparams로 카테고리 값 추가

--- apps/pick-learn/src/app/(home)/page.tsx
+++ apps/pick-learn/src/app/(home)/page.tsx
@@ -4,12 +4,18 @@ import StepSection from '@/views/home/ui/StepSection';
 import TopAnswererSection from '@/views/home/ui/TopAnswererSection';
 import TopAskSection from '@/views/home/ui/TopAskSection';
 
-export default function Home() {
+export default async function Home({
+    searchParams,
+}: {
+    searchParams: Promise<{ category: string }>;
+}) {
+    const { category } = await searchParams;
+
     return (
         <MainWrapper className='min-h-screen '>
             <HeroSection />
             <StepSection />
-            <TopAskSection />
+            <TopAskSection category={category} />
             <TopAnswererSection />
         </MainWrapper>
     );
