22b99cf53a375daede603da2fcdc5b08e6b8ed0a
Sutaenghhh <neocop7@gmail.com>
2025-06-04 17:52:19 +0900

    refactor: topAnswererSection css 수정

--- apps/pick-learn/src/views/home/ui/TopAnswererSection.tsx
+++ apps/pick-learn/src/views/home/ui/TopAnswererSection.tsx
@@ -1,20 +1,20 @@
 import Heading from '@/widgets/home/ui/Heading';
 import TopAnswererCardList from './TopAnswererCardList';
+import StepWrapper from '@/shared/ui/Wrapper/StepWrapper';
 
-//FIXME: 디자인에맞게 css 수정 필요
 export default function TopAnswererSection() {
     return (
-        <section className='min-h-screen space-y-10 my-25'>
+        <StepWrapper>
             <Heading align='center' className='space-y-4'>
                 <Heading.Title>
                     Top Answerer <br />
                     Hiring Now!
                 </Heading.Title>
-                <Heading.SubTitle>
+                <Heading.SubTitle className='mb-20'>
                     답을 찾아 드리는 핵답러들!!!
                 </Heading.SubTitle>
             </Heading>
             <TopAnswererCardList />
-        </section>
+        </StepWrapper>
     );
 }
