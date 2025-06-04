9c438924ac75acf8c8b87b5acb41ef2febc60867
Sutaenghhh <neocop7@gmail.com>
2025-06-04 09:49:06 +0900

    refactor: 태그 수정

--- apps/pick-learn/src/entities/category/ui/TopAskSubCategory.tsx
+++ apps/pick-learn/src/entities/category/ui/TopAskSubCategory.tsx
@@ -2,10 +2,8 @@ import { SubCategoryType } from '../api/types';
 
 export default function TopAskSubCategory({ item }: { item: SubCategoryType }) {
     return (
-        <ul>
-            <li className='text-sm bg-point-green-200/20 text-point-green-200 rounded-full w-fit py-1 px-1.5 my-5'>
-                {item.name}
-            </li>
-        </ul>
+        <span className='text-sm bg-point-green-200/20 text-point-green-200 rounded-full w-fit py-1 px-1.5 my-5'>
+            {item.name}
+        </span>
     );
 }
