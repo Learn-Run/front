36bc8aa23854ae36f2a39fa17f7a20e573b08bbc
Dora_bang <psa1195@naver.com>
2025-06-05 15:58:42 +0900

    fix: 공통 ui 컴포넌트 barrel export 추가

--- apps/pick-learn/src/shared/ui/index.ts
+++ apps/pick-learn/src/shared/ui/index.ts
@@ -2,5 +2,7 @@ import Label from './Label';
 import Tag from './Tag';
 import MainWrapper from './Wrapper/MainWrapper';
 import StepWrapper from './Wrapper/StepWrapper';
+import StatusCheckIcon from './StatusCheckIcon';
+import PasswordInput from './PasswordInput';
 
-export { Label, Tag, MainWrapper, StepWrapper };
+export { Label, Tag, MainWrapper, StepWrapper, StatusCheckIcon, PasswordInput };
