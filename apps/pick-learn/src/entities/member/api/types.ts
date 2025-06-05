715ecbde222d6ce90e088380d73d10e273548662
Sutaenghhh <neocop7@gmail.com>
2025-06-05 09:00:45 +0900

    refactor: 메인 페이지 퍼블리싱 수정  (#67)
    * refactor: topAnswererSection css 수정
    
    * refactor: topAskSection 프로필 사진 수정
    
    * refactor: topAskSection css 수정

--- apps/pick-learn/src/entities/member/api/types.ts
+++ apps/pick-learn/src/entities/member/api/types.ts
@@ -16,30 +16,26 @@ export interface TopAnswererType {
 export const topAnswererList: TopAnswererType[] = [
     {
         memberUuid: '1',
-        profileImage: 'https://picsum.photos/400/400',
+        profileImage:
+            'https://cdn.pixabay.com/photo/2025/03/30/13/01/fairy-tale-9502808_1280.jpg',
         nickname: '핵답러는 나다',
         alt: '핵답러 프로필 이미지',
         selfintroduction: '안녕하세요! 핵답러입니다.',
     },
     {
         memberUuid: '2',
-        profileImage: 'https://picsum.photos/400/400',
+        profileImage:
+            'https://cdn.pixabay.com/photo/2023/09/21/11/30/cat-8266486_1280.jpg',
         nickname: '핵답러',
         alt: '핵답러 프로필 이미지',
         selfintroduction: '열심히 답변하겠습니다.',
     },
-    // {
-    //     memberUuid: '3',
-    //     profileImage: 'https://picsum.photos/400/500',
-    //     nickname: '핵답러',
-    //     alt: '핵답러 프로필 이미지',
-    //     selfintroduction: '최선을 다해 답변드리겠습니다.',
-    // },
-    // {
-    //     memberUuid: '4',
-    //     profileImage: 'https://picsum.photos/200/64',
-    //     nickname: '핵답러',
-    //     alt: '핵답러 프로필 이미지',
-    //     selfintroduction: '성실하게 답변하겠습니다.',
-    // },
+    {
+        memberUuid: '3',
+        profileImage:
+            'https://cdn.pixabay.com/photo/2025/05/23/08/54/girl-9617241_1280.png',
+        nickname: '핵답러',
+        alt: '핵답러 프로필 이미지',
+        selfintroduction: '최선을 다해 답변드리겠습니다.',
+    },
 ];
