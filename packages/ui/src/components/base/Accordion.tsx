befa99a1d43ba8c5522f313127da29f716da38f3
Sutaenghhh <neocop7@gmail.com>
2025-06-08 20:05:39 +0900

    refactor: accordion 커스텀 전 상태로 초기화

--- packages/ui/src/components/base/Accordion.tsx
+++ packages/ui/src/components/base/Accordion.tsx
@@ -2,10 +2,9 @@
 
 import * as React from 'react';
 import * as AccordionPrimitive from '@radix-ui/react-accordion';
+import { ChevronDownIcon } from 'lucide-react';
 
 import { cn } from '../../lib/utils';
-import Minus from '../../assets/icons/Minus';
-import Plus from '../../assets/icons/Plus';
 
 function Accordion({
     ...props
@@ -20,11 +19,7 @@ function AccordionItem({
     return (
         <AccordionPrimitive.Item
             data-slot='accordion-item'
-            className={cn(
-                'border border-gray-400 rounded-lg px-5',
-                'data-[state=open]:bg-gray-100',
-                className,
-            )}
+            className={cn('border-b last:border-b-0', className)}
             {...props}
         />
     );
@@ -40,22 +35,13 @@ function AccordionTrigger({
             <AccordionPrimitive.Trigger
                 data-slot='accordion-trigger'
                 className={cn(
-                    'flex flex-1 items-start justify-between gap-4 rounded-md py-4',
-                    'text-left text-xl font-medium transition-all outline-none disabled:pointer-events-none',
-                    'group',
+                    'focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-md py-4 text-left text-sm font-medium transition-all outline-none hover:underline focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>svg]:rotate-180',
                     className,
                 )}
                 {...props}
             >
                 {children}
-                <Plus
-                    data-slot='plus'
-                    className='group-data-[state=open]:hidden'
-                />
-                <Minus
-                    data-slot='minus'
-                    className=' group-data-[state=closed]:hidden'
-                />
+                <ChevronDownIcon className='text-muted-foreground pointer-events-none size-4 shrink-0 translate-y-0.5 transition-transform duration-200' />
             </AccordionPrimitive.Trigger>
         </AccordionPrimitive.Header>
     );
@@ -69,7 +55,7 @@ function AccordionContent({
     return (
         <AccordionPrimitive.Content
             data-slot='accordion-content'
-            className='data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden text-gray-800'
+            className='data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden text-sm'
             {...props}
         >
             <div className={cn('pt-0 pb-4', className)}>{children}</div>
