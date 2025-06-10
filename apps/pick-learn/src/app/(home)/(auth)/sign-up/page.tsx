b29defe89a655754f14dd9ecedaad7562a802178
Dora_bang <39180932+Dorabang@users.noreply.github.com>
2025-06-10 17:55:47 +0900

    fix: sign up validate (#115)
    * refactor: mainWrapper에 상단 padding 값 추가
    
    * fix: 회원가입 중 모든 필드를 입력 후 api 중복 검증 시에도 다음 단계로 넘어갈 수 있도록 수정
    
    - shoudValidate 옵션을 통해서 값 변경 시 validate 검증 실행
    
    * fix: 이메일 입력 전 에러 설정되지 않도록 설정 추가
    
    * fix: mainWrapper에 상단 padding css 추가

--- apps/pick-learn/src/app/(home)/(auth)/sign-up/page.tsx
+++ apps/pick-learn/src/app/(home)/(auth)/sign-up/page.tsx
@@ -3,7 +3,7 @@ import SignUpContainer from '@/views/auth/ui/SignUpContainer';
 
 export default function SignUpPage() {
     return (
-        <MainWrapper>
+        <MainWrapper className='pt-40'>
             <SignUpContainer />
         </MainWrapper>
     );
