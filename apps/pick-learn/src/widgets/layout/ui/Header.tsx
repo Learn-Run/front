4d2ec6255fced9f3ac719e9840b02dd485f0fd28
Sutaenghhh <neocop7@gmail.com>
2025-06-10 16:12:46 +0900

    fix: header 오류 수정 (#109)

--- apps/pick-learn/src/widgets/layout/ui/Header.tsx
+++ apps/pick-learn/src/widgets/layout/ui/Header.tsx
@@ -11,10 +11,10 @@ export default function Header() {
     return (
         <header
             className={cn(
-                'fixed  md:top-10 z-50 w-full md:w-[calc(100%-80px)] max-w-[1440px] md:rounded-full bg-white md:shadow-md md:mx-10 ',
+                'fixed md:top-10 z-50 w-full md:w-[calc(100%-80px)] max-w-[1440px] md:rounded-full bg-white md:shadow-md md:mx-10 2xl:left-1/2 2xl:-translate-x-1/2',
             )}
         >
-            <div className='grid grid-cols-12 items-center container mx-auto px-5 sm:px-10 2xl:px-0 h-[5rem]'>
+            <div className='grid grid-cols-12 items-center container mx-auto px-5 sm:px-10 h-[5rem]'>
                 <Link
                     href='/'
                     className='col-span-6 md:col-span-2 xl:col-span-3'
