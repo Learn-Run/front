a28fdb27fde905a2e925c03ddddbfe9f371a2eed
Sutaenghhh <neocop7@gmail.com>
2025-06-11 11:04:34 +0900

    fix: 서브카테고리중 전체 서브카테고리는 선택된 메인 카테고리의 전체만 css다르도록 수정

--- apps/pick-learn/src/entities/category/ui/CategoryListItem.tsx
+++ apps/pick-learn/src/entities/category/ui/CategoryListItem.tsx
@@ -48,7 +48,7 @@ export default async function CategoryListItem({
                         </AccordionTrigger>
 
                         <SubCategoryItem
-                            mainCategoryId={item.id}
+                            mainCategoryId={mainCategoryId}
                             subCategoryId={subCategoryId}
                             mainCategoryItem={item.id}
                         />
