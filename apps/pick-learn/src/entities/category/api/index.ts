43059654374c7b896be4053f43ff39572d070638
Sutaenghhh <neocop7@gmail.com>
2025-06-11 11:03:57 +0900

    feat: 카테고리 더미 생성 버튼 추가

--- apps/pick-learn/src/entities/category/api/index.ts
+++ apps/pick-learn/src/entities/category/api/index.ts
@@ -1,21 +1,15 @@
 'use server';
 
 import { fetchData } from '@/shared/api/instance';
-import { CategoryListType, MainCategoryType, SubCategoryType } from './types';
+import { CategoryListType, MainCategoryType } from './types';
 import { services } from '@/shared/api/constants';
 
 export const getMainCategories = async (): Promise<MainCategoryType[]> => {
     const response = await fetchData.get<MainCategoryType[]>(
         `${services.post}/api/v1/category/main`,
-    );
-    return response.result;
-};
-
-export const getSubCategories = async (
-    mainCategoryId: number,
-): Promise<SubCategoryType[]> => {
-    const response = await fetchData.get<SubCategoryType[]>(
-        `${services.post}/api/v1/category/main/${mainCategoryId}`,
+        {
+            cache: 'force-cache',
+        },
     );
     return response.result;
 };
@@ -25,6 +19,35 @@ export const getSubCategoryListByMainCategoryId = async (
 ): Promise<CategoryListType[]> => {
     const response = await fetchData.get<CategoryListType[]>(
         `${services.post}/api/v1/category-list/main/${mainCategoryId}`,
+        {
+            cache: 'force-cache',
+        },
     );
+
     return response.result;
+};
+
+export const createMainCategory = async (
+    name: string,
+    iconUrl: string,
+    alt: string,
+) => {
+    await fetchData.post(`${services.post}/api/v1/category/main`, {
+        body: JSON.stringify({ name, iconUrl, alt }),
+    });
+};
+
+export const createSubCategory = async (name: string, color: string) => {
+    await fetchData.post(`${services.post}/api/v1/category/sub`, {
+        body: JSON.stringify({ name, color }),
+    });
+};
+
+export const createCategoryList = async (
+    mainCategoryId: number,
+    subCategoryId: number,
+) => {
+    await fetchData.post(`${services.post}/api/v1/category-list`, {
+        body: JSON.stringify({ mainCategoryId, subCategoryId }),
+    });
 };
