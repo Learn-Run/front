2d8609229712a69415259eb7adda0ca486478041
Dora_bang <psa1195@naver.com>
2025-06-12 12:24:35 +0900

    refactor: 불필요한 SignUpStateType 타입 삭제

--- apps/pick-learn/src/features/auth/model/types.ts
+++ apps/pick-learn/src/features/auth/model/types.ts
@@ -1,25 +1,11 @@
 export interface SignUpStepsType {
     id: number;
-    isEnable: boolean;
     requiredFields: string[];
     component: (args: SignUpStepProps) => React.ReactNode;
 }
 
 export interface SignUpStepProps {
     step: number;
-}
-
-export interface SignUpStateType {
-    nickname: string;
-    loginId: string;
-    password: string;
-    passwordConfirm: string;
-    email: string;
-    verificationCode: string;
-    isEmailVerified: boolean;
-    gender: GenderType;
-    birthDate: Date;
-    agreementCheckList: string[];
 }
 
 export type GenderType = '여성' | '남성';
