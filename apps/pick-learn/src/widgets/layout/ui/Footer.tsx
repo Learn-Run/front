c2c99022b346da5ec04440722f0100f51d94da1d
Sutaenghhh <neocop7@gmail.com>
2025-06-13 13:04:01 +0900

    refactor: footer border css 수정

--- apps/pick-learn/src/widgets/layout/ui/Footer.tsx
+++ apps/pick-learn/src/widgets/layout/ui/Footer.tsx
@@ -3,10 +3,9 @@ import { Logo } from '@/shared/assets/icons';
 export default function Footer() {
     return (
         <footer className=' w-full bg-black mb-[57px] md:mb-0'>
-            <div className='flex items-start container mx-auto py-6 px-5 '>
+            <div className='flex items-start container mx-auto border-b border-gray-700 py-6 px-5 '>
                 <Logo className='max-w-[100px]' />
             </div>
-            <hr className='border border-gray-700' />
             <div className='flex justify-between container mx-auto py-8 text-white text-sm flex-wrap md:flex-nowrap px-5 '>
                 <p>© 2025 Ferezob Job Finder. All rights reserved.</p>
                 <p>Terms & Conditions | Privacy Policy</p>
