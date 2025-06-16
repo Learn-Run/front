a23a3c593abf2f5eca1d19633b43dbf3533c180c
Dora_bang <psa1195@naver.com>
2025-06-16 14:12:03 +0900

    feat: nav에 필요한 assets 파일 추가

---
+++ apps/chat-service/src/shared/assets/icons/Overview.tsx
@@ -0,0 +1,41 @@
+export default function Overview({ className }: { className?: string }) {
+    return (
+        <svg
+            width='28'
+            height='28'
+            viewBox='0 0 28 28'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+            className={className}
+        >
+            <path
+                d='M11.6667 3.5H3.5V11.6667H11.6667V3.5Z'
+                stroke='currentColor'
+                strokeWidth='2'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+            />
+            <path
+                d='M26.4963 1.65723H16.4963V11.6572H26.4963V1.65723Z'
+                stroke='currentColor'
+                strokeWidth='2'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+            />
+            <path
+                d='M24.5 16.3333H16.3334V24.4999H24.5V16.3333Z'
+                stroke='currentColor'
+                strokeWidth='2'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+            />
+            <path
+                d='M11.6667 16.3333H3.5V24.4999H11.6667V16.3333Z'
+                stroke='currentColor'
+                strokeWidth='2'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+            />
+        </svg>
+    );
+}
