7e0979f3d64fdcb17a606706cca0316a9970ab74
Sutaenghhh <neocop7@gmail.com>
2025-06-16 09:59:30 +0900

    chore: 코드리뷰 수정사항

--- apps/pick-learn/src/app/(home)/profile/page.tsx
+++ apps/pick-learn/src/app/(home)/profile/page.tsx
@@ -1,12 +1,13 @@
 import { MainWrapper } from '@/shared/ui';
-import ProfileImageSection from '@/views/profile/ui/ProfileImageSection';
-import ProfileMenuSection from '@/views/profile/ui/ProfileMenuSection';
+// import ProfileImageSection from '@/views/profile/ui/ProfileImageSection';
+// import ProfileMenuSection from '@/views/profile/ui/ProfileMenuSection';
 
 export default function page() {
     return (
         <MainWrapper className='bg-gradient-to-b from-secondary-100 to-[#f7f2f3] pt-[8rem]'>
-            <ProfileImageSection />
-            <ProfileMenuSection />
+            {/* <ProfileImageSection />
+            <ProfileMenuSection /> */}
+            수정예정
         </MainWrapper>
     );
 }
--- apps/pick-learn/src/views/home/model/constants.ts
+++ apps/pick-learn/src/views/home/model/constants.ts
@@ -1,12 +1,12 @@
 import { Arts, Cook, Fashion, Interior, IT, Jobs } from '@/shared/assets/icons';
 
 export const iconMapById: Record<number, React.FC<{ className?: string }>> = {
-    1: Arts,
-    2: IT,
-    3: Jobs,
-    4: Cook,
-    5: Interior,
-    6: Fashion,
+    3: Arts,
+    4: IT,
+    7: Jobs,
+    8: Cook,
+    9: Interior,
+    10: Fashion,
 };
 
 export const videoUrlList = ['videos/v001.mp4', 'videos/v002.mp4'];
