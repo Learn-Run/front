036dc11a2c04905df656424ac1d5449cbf6b6dfb
Sutaenghhh <neocop7@gmail.com>
2025-06-16 09:57:20 +0900

    chore: 코드리뷰 수정사항

--- apps/pick-learn/src/widgets/layout/ui/ProfileButton.tsx
+++ apps/pick-learn/src/widgets/layout/ui/ProfileButton.tsx
@@ -1,14 +1,12 @@
+import Link from 'next/link';
+
 import Profile from '@/shared/assets/icons/Profile';
 import { routes } from '@/shared/model/constants/routes';
-import Link from 'next/link';
-import React from 'react';
 
 export default function ProfileButton() {
     return (
-        <button>
-            <Link href={routes.profile}>
-                <Profile />
-            </Link>
-        </button>
+        <Link href={routes.profile}>
+            <Profile />
+        </Link>
     );
 }
