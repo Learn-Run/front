2a169f6fcb056e77cb8e8e828506f9e8cfd3e6c4
Dora_bang <psa1195@naver.com>
2025-06-17 16:35:40 +0900

    feat: profile 컴포넌트 추가
    - 이후 API 연결 시 받을 Profile 타입 선언

---
+++ apps/chat-service/src/entities/profile/ui/Profile.tsx
@@ -0,0 +1,47 @@
+import { cn } from '@repo/ui/src/lib/utils';
+import { Avatar } from '@/entities/profile/ui';
+import { poppins } from '@/shared/assets/fonts';
+import { ProfileType } from '../model/types';
+
+export default function Profile({
+    profile,
+    className,
+}: {
+    profile?: ProfileType;
+    className?: string;
+}) {
+    if (!profile) return;
+
+    return (
+        <div
+            className={cn(
+                'flex gap-x-3 justify-between items-center min-w-[210px] w-full',
+                className,
+            )}
+        >
+            <Avatar
+                src={profile.profileImage.imageUrl}
+                alt={`${profile.nickname} 프로필 이미지`}
+            />
+
+            <div className='space-y-1 flex-grow'>
+                <p
+                    className={cn(
+                        poppins.className,
+                        'text-gray-900 font-medium inline-block text-sm',
+                    )}
+                >
+                    {profile.nickname}
+                </p>
+                <p
+                    className={cn(
+                        poppins.className,
+                        'text-gray-500 font-medium text-xs',
+                    )}
+                >
+                    {profile.gradeName}
+                </p>
+            </div>
+        </div>
+    );
+}
