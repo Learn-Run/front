f44024bc812f67694aaf09786178abedb0b16bb8
Sutaenghhh <neocop7@gmail.com>
2025-06-17 15:29:26 +0900

    refactor: css 수정

--- apps/pick-learn/src/views/home/ui/StepSection.tsx
+++ apps/pick-learn/src/views/home/ui/StepSection.tsx
@@ -8,7 +8,7 @@ import StepCarouselList from '@/entities/guide/ui/StepCarouseList';
 export default function StepSection() {
     return (
         <StepWrapper>
-            <div className='grid grid-cols-1 lg:grid-cols-12 container mx-auto px-4 md:px-0 justify-center lg:justify-start gap-10 lg:gap-20'>
+            <div className='grid grid-cols-1 lg:grid-cols-12 container mx-auto px-4 md:px-0 justify-center lg:justify-start gap-10 lg:gap-20 max-w-[1240px]'>
                 <Heading className='col-span-1 lg:col-span-5 xl:col-span-4 flex flex-col items-center lg:items-start gap-2'>
                     <Tag>How it Work</Tag>
                     <Heading.Title className='w-[80%] text-3xl md:w-[60%] lg:w-full text-center lg:text-left'>
