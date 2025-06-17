35f71452db4409356a9be1326b5590d02e0ca769
Sutaenghhh <neocop7@gmail.com>
2025-06-17 17:55:49 +0900

    refactor: 작성자 프로필 type 및 게시물 list type  수정  (#157)
    * refactor: 작성자 프로필 type 수정 및 grade 추가
    
    * refactor: 게시물listtype 수정

--- apps/pick-learn/src/entities/member/ui/Profile.tsx
+++ apps/pick-learn/src/entities/member/ui/Profile.tsx
@@ -11,22 +11,27 @@ interface ProfileProps {
 
 export default async function Profile({ memberUuid, className }: ProfileProps) {
     if (!memberUuid) return;
+
     const member = await getWrtierProfileByMemberUuid(memberUuid);
     const fallbackImage = S3_BASE_URL + 'baseprofile.webp';
     const imageUrl = member?.profileImage?.imageUrl || fallbackImage;
     const alt = member?.profileImage?.alt || member?.nickname;
 
     if (!member) return;
+
     return (
         <div className={cn('flex gap-x-2.5 items-center w-full', className)}>
             <Image
-                className='w-7 h-7 rounded-full bg-gray-600/20'
+                className='w-7 h-7 rounded-full bg-gray-600/20 object-cover'
                 src={imageUrl}
                 alt={alt || ''}
                 width={28}
                 height={28}
             />
-            <p>{member?.nickname}</p>
+            <p className='font-medium'>{member?.nickname}</p>
+            <p className='text-xs font-medium text-gray-500'>
+                {member?.gradeName}
+            </p>
         </div>
     );
 }
--- apps/pick-learn/src/entities/profile/api/types.ts
+++ apps/pick-learn/src/entities/profile/api/types.ts
@@ -14,5 +14,6 @@ export interface ProfileType {
 export interface WriterProfileType {
     memberUuid: string;
     nickname: string;
+    gradeName: string;
     profileImage: ProfileImageType;
 }
