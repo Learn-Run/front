4edefcee11bef9e8618dfb2cbd41be2e2aec1da8
Dora_bang <psa1195@naver.com>
2025-06-17 17:31:15 +0900

    feat: 화상 채팅방 Heading component 추가
    - 더미 데이터로 퍼블리싱 진행
    - 반응형 css 추가

---
+++ apps/chat-service/src/views/meeting/ui/MeetingDetailHeading.tsx
@@ -0,0 +1,41 @@
+import { MEETING_DATA } from '@/app/(home)/meeting/page';
+import Heading from '@/shared/ui/Heading';
+import { dateFormat } from '@/shared/utils/dateFormat';
+import MeetingParticipants from './MeetingParticipants';
+import { Profile } from '@/entities/profile/ui';
+import { PROFILE } from '@/app/(home)/meeting/[id]/page';
+import VerticalEllipsis from '@/shared/assets/icons/VerticalEllipsis';
+
+export default function MeetingDetailHeading({
+    meetingData,
+}: {
+    meetingData: (typeof MEETING_DATA)[number];
+}) {
+    const profile = PROFILE;
+
+    return (
+        <Heading className='p-6 bg-white border-b border-gray-300 pb-4 flex flex-wrap gap-4 justify-between items-center'>
+            <div>
+                <Heading.Title className='pb-2.5'>
+                    {meetingData?.title}
+                </Heading.Title>
+                <Heading.Description className='text-gray-500'>
+                    {dateFormat(meetingData.createdAt)}
+                </Heading.Description>
+            </div>
+
+            <div className='flex items-center gap-4'>
+                <MeetingParticipants participants={meetingData.participants} />
+
+                <Profile
+                    profile={profile}
+                    className='hidden lg:flex bg-gray-200 rounded-full p-3 items-center gap-2'
+                >
+                    <button>
+                        <VerticalEllipsis className='text-tertiary-100' />
+                    </button>
+                </Profile>
+            </div>
+        </Heading>
+    );
+}
