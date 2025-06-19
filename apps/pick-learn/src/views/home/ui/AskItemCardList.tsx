9eba836fbf18a4b4fa3983101e4bd5d01cb0da33
Sutaenghhh <neocop7@gmail.com>
2025-06-19 10:52:46 +0900

    feat: post 클릭 시 상세페이지로 이동할 수 있도록 추가

--- apps/pick-learn/src/views/home/ui/AskItemCardList.tsx
+++ apps/pick-learn/src/views/home/ui/AskItemCardList.tsx
@@ -1,9 +1,10 @@
-import { PostListType } from '@/entities/post/api/types';
+import Link from 'next/link';
+
 import { cn } from '@repo/ui/lib/utils';
+import { AskListType } from '@/entities/post/api/types';
+import { BookMark, Shared } from '@/shared/assets/icons';
 import TopAskSubCategory from '@/entities/category/ui/TopAskSubCategory';
 import Profile from '@/entities/member/ui/Profile';
-import BookMark from '@/shared/assets/icons/BookMark';
-import Shared from '@/shared/assets/icons/Shared';
 import PostEmptySection from '@/shared/ui/PostEmptySection';
 
 export default async function AskItemCardList({
@@ -11,7 +12,7 @@ export default async function AskItemCardList({
     postList,
 }: {
     className?: string;
-    postList?: PostListType;
+    postList?: AskListType;
 }) {
     if (!postList || postList.posts.length === 0) return <PostEmptySection />;
 
@@ -19,29 +20,31 @@ export default async function AskItemCardList({
         <div className='flex flex-col items-center justify-center w-full'>
             <ul
                 className={cn(
-                    'grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 container mx-auto md:px-0 items-center justify-center md:max-w-[80%] xl:max-w-[1262px] w-full',
+                    'grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 container mx-auto md:px-0 items-center justify-center md:max-w-[80%] xl:max-w-[1262px]',
                     className,
                 )}
             >
                 {postList.posts.map((item) => (
-                    <li
-                        key={item.postUuid}
-                        className='flex flex-col items-start justify-between border border-gray-400 rounded-2xl h-50 w-full px-5 py-6'
-                    >
-                        <div className='flex justify-between w-full'>
-                            <Profile memberUuid={item.memberUuid} />
-                            <div className='flex gap-x-2.5 items-center'>
-                                <BookMark />
-                                <Shared />
+                    <li key={item.postUuid}>
+                        <Link
+                            href={`/post/${item.postUuid}`}
+                            className='flex flex-col items-start justify-between border border-gray-400 rounded-2xl h-50 w-full px-5 py-6'
+                        >
+                            <div className='flex justify-between w-full'>
+                                <Profile memberUuid={item.memberUuid} />
+                                <div className='flex gap-x-2.5 items-center'>
+                                    <BookMark />
+                                    <Shared />
+                                </div>
                             </div>
-                        </div>
-                        <TopAskSubCategory item={item.subCategoryId} />
-                        <h3 className='text-lg font-medium truncate w-full'>
-                            {item.title}
-                        </h3>
-                        <p className='text-sm text-gray-700 truncate w-full'>
-                            {item.contents}
-                        </p>
+                            <TopAskSubCategory item={item.subCategoryId} />
+                            <h3 className='text-lg font-medium truncate w-full'>
+                                {item.title}
+                            </h3>
+                            <p className='text-sm text-gray-700 truncate w-full'>
+                                {item.contents}
+                            </p>
+                        </Link>
                     </li>
                 ))}
             </ul>
