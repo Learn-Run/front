bbd7ca6546fd21120fef9f806b41251bcfb80358
Sutaenghhh <neocop7@gmail.com>
2025-06-19 13:52:52 +0900

    fix: postDetail/ui 폴더명 대소문자 정리

--- apps/pick-learn/src/views/PostDetail/ui/PostDetailContentSection.tsx
+++
@@ -1,9 +0,0 @@
-import SectionWrapper from '@/shared/ui/wrapper/SectionWrapper';
-
-export default function PostDetailContentSection({
-    contents,
-}: {
-    contents: string;
-}) {
-    return <SectionWrapper className='pt-11'>{contents}</SectionWrapper>;
-}
