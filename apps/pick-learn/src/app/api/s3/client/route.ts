d48741b64a2e4b9ab95e8ca54085f496018d3c33
Sutaenghhh <neocop7@gmail.com>
2025-06-20 15:58:14 +0900

    feat: s3 routes.ts 추가

---
+++ apps/pick-learn/src/app/api/s3/client/route.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+
+const s3Client = new S3Client({
+    region: process.env.AWS_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    },
+});
+
+export async function POST(req: NextRequest) {
+    try {
+        const { fileName, fileType, fileContent } = await req.json();
+        console.log('Received fileName:', fileName);
+        const params = {
+            Bucket: process.env.AWS_BUCKET_NAME!,
+            Key: `post/${Date.now()}-${fileName}`,
+            Body: Buffer.from(fileContent, 'base64'),
+            ContentType: fileType,
+        };
+
+        const command = new PutObjectCommand(params);
+        await s3Client.send(command);
+
+        const imageUrl = `${process.env.AWS_CLOUDFRONT_DOMAIN}/${params.Key}`;
+        console.log('File uploaded successfully:', imageUrl);
+        return NextResponse.json({ imageUrl });
+    } catch (error) {
+        console.error('Error uploading to S3:', error);
+        return NextResponse.json(
+            { error: 'Failed to upload file' },
+            { status: 500 },
+        );
+    }
+}
