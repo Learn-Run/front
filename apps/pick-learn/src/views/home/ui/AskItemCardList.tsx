f85185cf1308b329baab709fc95f08121ccd8a64
Sutaenghhh <neocop7@gmail.com>
2025-06-21 17:06:03 +0900

    fix: 게시물 목록 수정

--- apps/pick-learn/src/views/home/ui/AskItemCardList.tsx
+++ apps/pick-learn/src/views/home/ui/AskItemCardList.tsx
@@ -1,12 +1,13 @@
 import Link from 'next/link';
+import { htmlToText } from 'html-to-text';
 
 import { cn } from '@repo/ui/lib/utils';
+import { routes } from '@/shared/model/constants/routes';
 import { AskListType } from '@/entities/post/api/types';
 import { BookMark, Shared } from '@/shared/assets/icons';
 import TopAskSubCategory from '@/entities/category/ui/TopAskSubCategory';
 import Profile from '@/entities/member/ui/Profile';
 import PostEmptySection from '@/shared/ui/PostEmptySection';
-import { routes } from '@/shared/model/constants/routes';
 
 export default async function AskItemCardList({
     className,
@@ -21,7 +22,7 @@ export default async function AskItemCardList({
         <div className='flex flex-col items-center justify-center w-full'>
             <ul
                 className={cn(
-                    'grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 container mx-auto md:px-0 items-center justify-center md:max-w-[80%] xl:max-w-[1262px]',
+                    'grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 md:px-0 items-center justify-center px-4 xl:max-w-[1262px]',
                     className,
                 )}
             >
@@ -44,8 +45,14 @@ export default async function AskItemCardList({
                             <h3 className='text-lg font-medium truncate w-full'>
                                 {item.title}
                             </h3>
+
                             <p className='text-sm text-gray-700 truncate w-full'>
-                                {item.contents}
+                                {htmlToText(item.contents, {
+                                    wordwrap: false,
+                                    selectors: [
+                                        { selector: 'img', format: 'skip' }, // 이미지 무시
+                                    ],
+                                })}
                             </p>
                         </Link>
                     </li>
