5bf0d21fdb654b9ee805360e4ae9ffb772552356
Sutaenghhh <neocop7@gmail.com>
2025-06-22 20:10:11 +0900

    chore: 코드리뷰 수정

--- apps/pick-learn/src/views/profile/ui/ProfileImage.tsx
+++ apps/pick-learn/src/views/profile/ui/ProfileImage.tsx
@@ -1,16 +1,14 @@
 import Image from 'next/image';
-import React from 'react';
+
 import { ProfileImageType } from '@/entities/profile/api/types';
 import { S3_BASE_URL } from '@/shared/model/constants';
-import { fallbackImageList } from './constants';
 
 export default function ProfileImage({
     profileImage,
 }: {
     profileImage: ProfileImageType;
 }) {
-    const randomIndex = Math.floor(Math.random() * fallbackImageList.length);
-    const fallbackImage = S3_BASE_URL + fallbackImageList[randomIndex];
+    const fallbackImage = S3_BASE_URL + 'baseprofile.webp';
     const imageUrl = profileImage?.imageUrl || fallbackImage;
     const alt = profileImage?.alt || profileImage?.alt + '프로필 이미지';
 
--- apps/pick-learn/src/views/profile/ui/constants.ts
+++
@@ -1,6 +0,0 @@
-export const fallbackImageList = [
-    'baseprofile.webp',
-    'profileImage/RandomProfileImage.jpg',
-    'profileImage/RandomProfileImage2.jpg',
-    'profileImage/RandomProfileImage3.jpg',
-];
