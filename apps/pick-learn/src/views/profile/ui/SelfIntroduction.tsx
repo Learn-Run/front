00919ac9bfc16443f0d5b38650a4d9b60a9e3e57
Sutaenghhh <neocop7@gmail.com>
2025-06-24 10:39:10 +0900

    refactor: test 코드 삭제

--- apps/pick-learn/src/views/profile/ui/SelfIntroduction.tsx
+++ apps/pick-learn/src/views/profile/ui/SelfIntroduction.tsx
@@ -7,7 +7,7 @@ export default function SelfIntroduction({
         <div className='flex flex-col gap-2'>
             <p className='font-semibold text-xl'>My Introduction</p>
             <p className='text-gray-700 border border-gray-700 rounded-xl w-full p-4 bg-white min-w-[230px] min-h-[100px]'>
-                {selfIntroduction} 가나다라마바사아차ㅏ
+                {selfIntroduction}
             </p>
         </div>
     );
