df5b9245e4f469552eb76a06dffd1a2ac75dafa2
Dora_bang <psa1195@naver.com>
2025-06-24 09:32:17 +0900

    fix: 페이지 구성이 변경되어 NAVIGATION_ITEMS 상수 수정

--- apps/chat-service/src/widgets/layout/model/constants.ts
+++ apps/chat-service/src/widgets/layout/model/constants.ts
@@ -1,8 +1,6 @@
 import Askers from '@/shared/assets/icons/Askers';
-import Meeting from '@/shared/assets/icons/Meeting';
 import Message from '@/shared/assets/icons/Message';
 import Overview from '@/shared/assets/icons/Overview';
-import Setting from '@/shared/assets/icons/Setting';
 import { routes } from '@/shared/model/constants/routes';
 
 export const NAVIGATION_ITEMS = [
@@ -14,31 +12,24 @@ export const NAVIGATION_ITEMS = [
         mobileHidden: false,
     },
     {
-        id: 2,
-        label: 'Meeting',
-        icon: Meeting,
-        href: routes.meeting,
-        mobileHidden: false,
-    },
-    {
         id: 3,
         label: 'Messages',
         icon: Message,
         href: routes.messages,
         mobileHidden: false,
     },
-    {
-        id: 4,
-        label: 'Askers',
-        icon: Askers,
-        href: routes.askers,
-        mobileHidden: false,
-    },
+    // {
+    //     id: 4,
+    //     label: 'Askers',
+    //     icon: Askers,
+    //     href: routes.askers,
+    //     mobileHidden: false,
+    // },
     {
         id: 5,
-        label: 'Setting',
-        icon: Setting,
-        href: routes.settings,
+        label: 'Profile',
+        icon: Askers,
+        href: routes.profile,
         mobileHidden: true,
     },
 ];
