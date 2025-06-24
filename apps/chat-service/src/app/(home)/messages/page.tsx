d1d1a7c74d512702a4f658ae377276edfcd1179c
Dora_bang <psa1195@naver.com>
2025-06-24 09:28:55 +0900

    refactor: 페이지 path 변경으로 인해 directory 명 변경

--- apps/chat-service/src/app/(home)/messages/page.tsx
+++ apps/chat-service/src/app/(home)/messages/page.tsx
@@ -1,3 +1,23 @@
-export default function MessagesPage() {
-    return <div>MessagesPage</div>;
+import ChatRoomListSection from '@/views/chatList/ui/ChatRoomListSection';
+import ChatRoomSection from '@/views/chatList/ui/ChatRoomSection';
+
+export default async function ChatRoomPage({
+    searchParams,
+}: {
+    searchParams: Promise<{ chatRoomUuid?: number }>;
+}) {
+    const { chatRoomUuid } = await searchParams;
+
+    return (
+        <main className='bg-gray-100 w-full h-dvh'>
+            <div className='grid grid-rows-2 md:grid-rows-1 md:grid-cols-3 h-full'>
+                <ChatRoomListSection />
+
+                <ChatRoomSection
+                    chatRoomUuid={chatRoomUuid}
+                    className='col-span-1 bg-blue-100 h-full p-6'
+                />
+            </div>
+        </main>
+    );
 }
