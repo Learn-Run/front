ae86d725651a6edf7db2f084b3a0247b2aa98ea1
김정환 <103404957+rlawjdghksdlqslek@users.noreply.github.com>
2025-06-27 10:26:47 +0900

    chore : Frontend Deploy to ECR and EC2 (#220)
    * test :  Frontend 배포 테스트
    
    * test 배포
    
    * 배포 test
    
    * test : deploy test
    
    * chore(deploy): test deploy setup
    
    * chore(deploy): test deploy setup
    
    * chore(deploy): test deploy setup
    
    * chore(deploy): test deploy setup
    
    * chore(deploy): ecr 주소 변경
    
    * chore(deploy): env 값 추가
    
    * chore(deploy): env 값 추가
    
    * chore(deploy): env 값 추가
    
    * chore(deploy): port 변경
    
    * chore(deploy): push branch 변경
    
    * chore(deploy): 불필요한 env 의존 제거
    
    * chore(deploy): chat next.config 경로 수정

--- apps/chat-service/next.config.ts
+++ apps/chat-service/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-    /* config options here */
+    basePath: '/chat',
+    assetPrefix: '/chat',
     images: {
         remotePatterns: [new URL('https://picsum.photos/**')],
     },
