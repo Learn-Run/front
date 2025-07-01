6ba2a9e199f99686bcc1cc40e2c45540588577ca
Sutaenghhh <neocop7@gmail.com>
2025-07-01 15:24:42 +0900

    fix: type오류 수정

--- apps/pick-learn/src/app/(home)/payment/success/page.tsx
+++ apps/pick-learn/src/app/(home)/payment/success/page.tsx
@@ -14,7 +14,7 @@ export default async function SuccessPage({
     const { orderId, amount, paymentKey } = await searchParams;
     const paymentConfirm = await getPaymentConfirm({
         orderId,
-        amount,
+        amount: Number(amount),
         paymentKey,
     });
 
