c84b4b0f966804887cf5bed20d8805bbce0b63b3
Sutaenghhh <neocop7@gmail.com>
2025-07-02 20:59:06 +0900

    refactor: css 수정

--- apps/chat-service/src/views/home/ui/MainTopSection.tsx
+++ apps/chat-service/src/views/home/ui/MainTopSection.tsx
@@ -5,7 +5,7 @@ import { SectionWrapper } from '@/shared/ui';
 export default function MainTopSection() {
     return (
         <SectionWrapper>
-            <div className='flex flex-cols-1 lg:flex-cols-2 mt-8 gap-x-2 border-b border-gray-300 pb-8'>
+            <div className='flex flex-col gap-y-5 lg:flex-row mt-8 gap-x-2 border-b border-gray-300 pb-8'>
                 <CurrentMessage />
                 <ActiveHistorySection />
             </div>
