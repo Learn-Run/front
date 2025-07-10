c59388542579895ac0fbef204c3e4bf2980853d0
Sutaenghhh <neocop7@gmail.com>
2025-07-10 14:30:48 +0900

    refactor: layout (#320)

--- apps/chat-service/src/views/home/ui/MainHeader.tsx
+++ apps/chat-service/src/views/home/ui/MainHeader.tsx
@@ -3,7 +3,7 @@ import { cn } from '@repo/ui/lib/utils';
 
 export default function MainHeader() {
     return (
-        <header className='border border-gray-300 p-8 bg-white '>
+        <header className='sticky top-0 left-60 right-0 border border-gray-300 p-8 bg-white hidden md:block z-10'>
             <h2 className='text-2xl font-medium pb-2.5'>활동 대시보드</h2>
             <p className={cn(poppins.className, 'text-sm text-gray-500')}>
                 최근 대화와 알림을 한 눈에 확인하세요
